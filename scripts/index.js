838f2cd32ce4194b3beb37708cc621abf625883a
Megan Jakims <meganjakims1@gmail.com>
2025-03-18 21:30:36 -0400

    Initialize array

---
+++ scripts/index.js
@@ -0,0 +1,28 @@
+const initialCards = [
+  {
+    name: "Beach",
+    link: "https://unsplash.com/photos/coconut-tree-on-beach-DH_u2aV3nGM?utm_content=creditShareLink&utm_medium=referral&utm_source=unsplash",
+  },
+  {
+    name: "Believe",
+    link: "https://unsplash.com/photos/toddler-looking-at-believe-in-yourself-graffiti-yMg_SMqfoRU?utm_content=creditShareLink&utm_medium=referral&utm_source=unsplash",
+  },
+  {
+    name: "Latte",
+    link: "https://unsplash.com/photos/cup-of-coffee-on-saucer-with-teaspoon-on-pink-tabletop-X2s8GhnQmds?utm_content=creditShareLink&utm_medium=referral&utm_source=unsplash",
+  },
+  {
+    name: "Marylin Monroe",
+    link: "https://unsplash.com/photos/woman-in-white-dress-sitting-on-chair-LvJvNPJlu9E?utm_content=creditShareLink&utm_medium=referral&utm_source=unsplash",
+  },
+  {
+    name: "Peace",
+    link: "https://unsplash.com/photos/woman-doing-yoga-meditation-on-brown-parquet-flooring-NTyBbu66_SI?utm_content=creditShareLink&utm_medium=referral&utm_source=unsplash",
+  },
+  {
+    name: "Sunflower",
+    link: "https://unsplash.com/photos/yellow-sunflower-in-bloom-during-daytime-Kh5uDUoXXfU?utm_content=creditShareLink&utm_medium=referral&utm_source=unsplash",
+  },
+];
+
+console.log(initialCards);
